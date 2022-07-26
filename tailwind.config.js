5425bcd2b4abbc1f38356a4e3b2ef456fb5561f5
Risqiyyyy <risqiyt12@gmail.com>
2022-07-26 20:41:08 +0700

    slide

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
+  content: ['index.html',"./node_modules/flowbite/**/*.js"],
   theme: {
     container:{
       center: true,
@@ -17,5 +17,5 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 }
